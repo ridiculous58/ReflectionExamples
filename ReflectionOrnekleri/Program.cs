d4f48117d4e2542244751bfa084af8e4c92d4b00
Emirhan <60364545+ridiculous58@users.noreply.github.com>
2020-11-05 09:08:18 +0300

    Reflection Examples

---
+++ ReflectionOrnekleri/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionOrnekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                
+                Name = "emirhan",
+                Age = "10"
+            };
+
+            Console.WriteLine(Control.Check(person));
+
+            Console.ReadLine();
+        }
+    }
+    public class Person
+    {
+        [Zorunlu]
+        public string Name;
+        [Zorunlu]
+        public int Id;
+        [Zorunlu]
+        public string Age;
+    }
+    public class ZorunluAttribute : Attribute { }
+
+
+    public class Control
+    {
+
+        public static bool Check(Person person)
+        {
+            Type type = typeof(Person);
+            foreach (var item in type.GetFields())
+            {
+                object[] attribute = item.GetCustomAttributes(typeof(ZorunluAttribute), false);
+
+                if (attribute.Length != 0)
+                {
+                    if (item.GetValue(person).GetType() == typeof(Int32))
+                    {
+                        if ((int)item.GetValue(person) == 0)
+                        {
+                            return false;
+                        }
+                    }
+                    if (item.GetValue(person) == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
